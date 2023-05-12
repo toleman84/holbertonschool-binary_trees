cb19514daa2df54539aef5339b9695297f2824d5
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-12 00:41:11 -0300

    add ej 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/*
+ * binary_tree_node - function for create a new tree.
+ * @parent: first member.
+ * @value: second member.
+ *
+ * Return: a new tree.
+ */
+
+ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+ {
+    binary_tree_t *newTree = NULL; // new tree
+
+    // malloc to storage in new tree
+    newTree = malloc(sizeof(binary_tree_t));
+    if (newTree == NULL)
+    {
+        return = (NULL);
+    }
+
+    if (parent != NULL)
+    {
+        newTree->parent = parent;
+    }
+    else
+    {
+        newTree->parent = NULL;
+    }
+
+    newTree->n = value;
+    newTree->left = NULL;
+    newTree->right = NULL;
+
+    return (newTree);
+ }
+ 
