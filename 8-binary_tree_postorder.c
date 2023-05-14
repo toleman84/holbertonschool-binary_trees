28dff1cccba72911b14ce7c8d2d2cc7ac63a9c02
toleman84 <gdponcemarsiglia@gmail.com>
2023-05-13 19:45:12 -0700

    fix betty ej 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - function that goes through a binary tree using post-order traversal
+ * binary_tree_postorder - binary tree using post-order traversal
  * @tree: Pointer to the node to insert the left-child in.
  * @func: pointer to a function to call for each node.
  *
@@ -10,10 +10,10 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree && func)
-    {
-        func(tree->n);
-        binary_tree_postorder(tree->left, func);
-        binary_tree_postorder(tree->right, func);
-    }
+	if (tree && func)
+	{
+		func(tree->n);
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+	}
 }
