87018d04fdd5824b5e5c9f02e1565e856f955f44
toleman84 <gdponcemarsiglia@gmail.com>
2023-05-13 18:03:23 -0700

    fix betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,25 +17,25 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-    //Where parent is a pointer to the node to insert the right-child in
-    //And value is the value to store in the new node
+	/*Where parent is a pointer to the node to insert the right-child in*/
+	/*And value is the value to store in the new node*/
 	newChild_right = binary_tree_node(parent, value);
 	if (newChild_right == NULL)
 	{
 		return (NULL);
 	}
 
-    //If parent already has a right-child, the new node must take its place
-    if (newChild_right)
+	/*If parent already has a right-child, the new node must take its place*/
+	if (newChild_right)
 	{
 		newChild_right->right = parent->right;
-        //the old right-child must be set as the right-child of the new node
+		/*the old right-child must be set as the right-child of the new node*/
 		if (parent->right)
 			parent->right->parent = newChild_right;
 		parent->right = newChild_right;
 	}
 
-    //Your function must return a pointer to the created node
-    //or NULL on failure or if parent is NULL
+	/*Your function must return a pointer to the created node*/
+	/*or NULL on failure or if parent is NULL*/
 	return (newChild_right);
 }
