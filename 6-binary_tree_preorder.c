7b3caf55e3eecfac3e9b98093632b9f8c0e49fa4
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-13 23:29:27 -0300

    fix ej 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,11 +9,7 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL && func == NULL)
-    {
-        return;
-    }
-    else
+    if (tree && func)
     {
         func(tree->n);
         binary_tree_preorder(tree->left, func);
