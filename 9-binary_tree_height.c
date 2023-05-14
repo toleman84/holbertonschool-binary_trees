4b6ab36ff4b667ff2f2d391ee04bcb89e08b0247
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-14 01:00:52 -0300

    fix ej 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,24 +9,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_side = NULL;
-    size_t right_side = NULL;
-
     if (tree == NULL)
     {
         return (0);
     }
-    else
+	else
 	{
-		if (left_side = tree->left)
-            return (1 + binary_tree_height(tree->left));
-        else
-            return (1);
-
-		if (right_side = tree->right)
-            return (1 + binary_tree_height(tree->right));
-        else
-            return (1);
+		size_t left_side = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		size_t right_side = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 		return (MAX(left_side, right_side));
 	}
