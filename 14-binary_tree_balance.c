0d0aa2bb53ad76347fb8c207826021abf560bb40
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-14 01:08:36 -0300

    fix ej 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+size_t binary_tree_height(const binary_tree_t *tree);
+
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
@@ -20,4 +22,28 @@ int binary_tree_balance(const binary_tree_t *tree)
     }
 
     return (0);
+}
+
+#include <sys/param.h>
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: binary_tree_height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+    {
+        return (0);
+    }
+	else
+	{
+		size_t left_side = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		size_t right_side = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		return (MAX(left_side, right_side));
+	}
 }
