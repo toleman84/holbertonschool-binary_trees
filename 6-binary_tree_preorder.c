5e411c9a9297e832a9be300a5732a9050b2aa28b
toleman84 <gdponcemarsiglia@gmail.com>
2023-05-13 19:33:01 -0700

    fix ej 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,18 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - binary tree using pre-order traversal
  * @tree: Pointer to the node to insert the left-child in.
+ * @func: pointer to a function to call for each node.
  *
  * Return: pointer to the created node, or NULL on failure or if parent is NULL
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree && func)
-    {
-        func(tree->n);
-        binary_tree_preorder(tree->left, func);
-        binary_tree_preorder(tree->right, func);
-    }
+	if (tree && func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
