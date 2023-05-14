0cdcfeaf92e01a0deb72b555c34a0d50e5c74c08
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-14 00:04:55 -0300

    fix ej 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -14,5 +14,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
         return (0);
     }
 
-    return (1 + binary_tree_depth(tree->parent));
+    return (binary_tree_depth(tree->parent));
 }
