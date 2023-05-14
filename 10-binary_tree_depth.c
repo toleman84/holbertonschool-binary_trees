7715d80387cf71c5b141b93b66d6b25c2ed8513e
toleman84 <gdponcemarsiglia@gmail.com>
2023-05-13 20:12:58 -0700

    fix betty ej 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,14 +9,15 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    if (tree->parent == NULL)
+	if (tree == NULL)
 	{
 		return (0);
 	}
 
-    return (1 + binary_tree_depth(tree->parent));
+	if (tree->parent == NULL)
+	{
+		return (0);
+	}
+
+	return (1 + binary_tree_depth(tree->parent));
 }
