04083c68c912e8f455c2f137d04c3e5eacd2cfc4
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-14 00:03:46 -0300

    add ej 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: Pointer to the node to insert the left-child in.
+ *
+ * Return: nothing
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+    {
+        return (0);
+    }
+
+    return (1 + binary_tree_depth(tree->parent));
+}
