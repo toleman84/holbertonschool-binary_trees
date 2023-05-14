58fbdd6864bb599baa1cb122284d3ca39c81b634
toleman84 <gdponcemarsiglia@gmail.com>
2023-05-13 21:14:24 -0700

    fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,10 +10,10 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 	else
 	{
 		size_t left_side = tree->left ? 1 + binary_tree_height(tree->left) : 0;
