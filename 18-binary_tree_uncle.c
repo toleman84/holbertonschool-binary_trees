1497ef91c39fd1a98968e6027c3a859e25e63224
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-15 16:01:04 -0300

    fix ej 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -33,7 +33,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
         return (NULL);
     }
 
-    node = node->parent;
+    // node = node->parent;
 
     if (node->parent->left != node)
     {
