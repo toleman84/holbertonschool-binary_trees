4ddd047d9946fbab34a68d431854d3d845de4ab2
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-15 15:47:41 -0300

    fix ej 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,3 +1,5 @@
+#include "binary_trees.h"
+
 /**
  * binary_tree_uncle - function that finds the uncle of a node
  * @node: pointer to the node to find the uncle
@@ -23,5 +25,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
         return (node->parent->right);
     }
 
-    // return (NULL);
+    return (NULL);
 }
