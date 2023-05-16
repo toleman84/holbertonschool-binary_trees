65b9872af19d2e08a4d44a04a6a376613db91837
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-05-15 20:57:52 -0700

    fixit

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,11 +7,13 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+size_t left_size, right_size;
+
 if (tree == NULL)
 return (0);
 
-size_t left_size = binary_tree_size(tree->left);
-size_t right_size = binary_tree_size(tree->right);
+left_size = binary_tree_size(tree->left);
+right_size = binary_tree_size(tree->right);
 
 return (left_size + right_size + 1);
 }
