eb96db4010c34ba4fda67d91f8d104a5f33edc86
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-05-15 21:00:31 -0700

    betu

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,7 +2,6 @@
 /**
 * binary_tree_size - measures the size of a binary tree
 * @tree: pointer to the root node of the tree to measure the size
-* 
 * Return: the size of the binary tree.
 */
 size_t binary_tree_size(const binary_tree_t *tree)
