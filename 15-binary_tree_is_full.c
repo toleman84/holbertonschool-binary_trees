754814a7b92269f3dc800e8f4b17b94f199ea122
toleman84 <gdponcemarsiglia@gmail.com>
2023-05-16 17:34:47 -0700

    fix betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,20 +9,20 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    if ((tree->left == NULL) && (tree->right == NULL))
-    {
-        return (1);
-    }
+	if ((tree->left == NULL) && (tree->right == NULL))
+	{
+		return (1);
+	}
 
-    if ((tree->left) && (tree->right))
-    {
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    }
+	if ((tree->left) && (tree->right))
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
 
-    return (0);
+	return (0);
 }
